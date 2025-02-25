11da40914e9ba0f8a59dd1f5c60a5320fa9df996
Viish <104511692+ViishSensei@users.noreply.github.com>
2025-02-25 16:50:59 +0530

    feat: add RTT command to calculate round-trip time and update exports

--- dokdo/src/commands/index.js
+++ dokdo/src/commands/index.js
@@ -1,8 +1,9 @@
-export * from './main.js';
-export * from './exec.js';
-export * from './js.js';
-export * from './shard.js';
-export * from './jsi.js';
-export * from './curl.js';
-export * from './cat.js';
+export * from "./main.js";
+export * from "./exec.js";
+export * from "./js.js";
+export * from "./rtt.js";
+export * from "./shard.js";
+export * from "./jsi.js";
+export * from "./curl.js";
+export * from "./cat.js";
 //# sourceMappingURL=index.js.map
---
+++ dokdo/src/commands/rtt.d.ts
@@ -0,0 +1,3 @@
+import type { Client, Context } from "../index.js";
+export declare function rtt(message: Context, parent: Client): Promise<void>;
+//# sourceMappingURL=rtt.d.ts.map
